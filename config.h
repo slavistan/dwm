ace55b2ab9e582a947703a1db451ff62377573ad
Stanislaw Hüll <st@nislaw.de>
2020-06-28 16:00:32 +0200

    Add class names for some floating windows

--- config.h
+++ config.h
@@ -33,11 +33,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Spacefm",  NULL,       NULL,       0,            1,           -1 },
-	{ "Yad",      NULL,       NULL,       0,            1,           -1 },
-	// { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class        , instance     , title , tags mask , isfloating , monitor , */
+	{ "Gimp"        , NULL         , NULL  , 0         , 1          , -1 }    ,
+	{ "Spacefm"     , NULL         , NULL  , 0         , 1          , -1 }    ,
+	{ "Yad"         , NULL         , NULL  , 0         , 1          , -1 }    ,
+  { "st-256color" , "st-float"   , NULL  , 0         , 1          , -1 }    ,
+  { "Sxiv"        , "sxiv-float" , NULL  , 0         , 1          , -1 }    ,
 };
 
 
