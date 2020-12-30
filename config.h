a3d4f29f9a166fb21554c9a2956b41777bf35f80
Stanislaw Hüll <st@nislaw.de>
2020-12-30 11:12:31 +0100

    Remove unused rules

--- config.h
+++ config.h
@@ -37,9 +37,6 @@ static const Rule rules[] = {
 	 */
 	/* class           , instance    , title, tags mask, isfloating, monitor */
 	{ "Gimp"           , NULL        , NULL , 0        , 1         , -1 },
-	{ "Spacefm"        , NULL        , NULL , 0        , 1         , -1 },
-	{ "Yad"            , NULL        , NULL , 0        , 1         , -1 },
-	{ "st-256color"    , "st-float"  , NULL , 0        , 1         , -1 },
 	{ "st-256color"    , "st-wiki"   , NULL , (1 << 8) , 0         , -1 },
 	{ "Microsoft Teams", NULL        , NULL , (1 << 7) , 0         , -1 },
 	{ "Firefox"        , NULL        , NULL , (1 << 1) , 0         , -1 },
